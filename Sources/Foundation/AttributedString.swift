d709253a514b7ef64c2b6efba02d3ba30d671424
Lee <18611401994@163.com>
2019-10-25 18:27:47 +0800

    add `NSAttributedString` `func applying(attributes: [NSAttributedString.Key: Any]) -> NSAttributedString `

--- Sources/Foundation/AttributedString.swift
+++ Sources/Foundation/AttributedString.swift
@@ -2,6 +2,13 @@ import Foundation
 
 extension NSAttributedString {
     
+    public func applying(attributes: [NSAttributedString.Key: Any]) -> NSAttributedString {
+        let copy = NSMutableAttributedString(attributedString: self)
+        let range = (string as NSString).range(of: string)
+        copy.addAttributes(attributes, range: range)
+        return copy
+    }
+    
     public static func + (lhs: NSAttributedString, rhs: String) -> NSAttributedString {
         guard !lhs.string.isEmpty else {
             return rhs.attributed()
