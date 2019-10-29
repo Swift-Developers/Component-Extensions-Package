8c9cb360dc28cde7bbeb06fede3dcdea0573e9a5
Lee <18611401994@163.com>
2019-10-29 16:31:03 +0800

    add `UIDevice.identifier`

---
+++ Sources/UIKit/Device.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIDevice {
+    
+    /// 设备标识 iPhone12,5 / iPad8,5
+    public static let identifier: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let mirror = Mirror(reflecting: systemInfo.machine)
+        
+        let identifier = mirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    } ()
+}
