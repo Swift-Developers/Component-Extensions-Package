0461e3493eb4d3b6105420022364c5c63c95bb82
Lee <18611401994@163.com>
2020-07-02 11:32:03 +0800

    优化NavigationController

--- Sources/UIKit/NavigationController.swift
+++ Sources/UIKit/NavigationController.swift
@@ -10,8 +10,8 @@ extension UINavigationController {
     
     /// 过滤所有符合类型的视图控制器
     /// - Parameter controller: 控制器类型
-    public func filter<T: UIViewController>(_ controller: T.Type) -> [T]? {
-        return viewControllers.filter { type(of: $0) == controller } as? [T]
+    public func filter<T: UIViewController>(_ controller: T.Type) -> [T] {
+        return viewControllers.filter { type(of: $0) == controller } as? [T] ?? []
     }
     
     /// 移除所有符合类型的视图控制器
@@ -19,10 +19,7 @@ extension UINavigationController {
     /// - Parameter animated: 是否动画
     @discardableResult
     public func remove<T: UIViewController>(_ controller: T.Type, animated: Bool = false) -> [T]? {
-        guard let controllers = filter(T.self) else {
-            return nil
-        }
-        
+        let controllers = filter(T.self)
         var temp = viewControllers
         temp.removeAll(controllers)
         setViewControllers(temp, animated: animated)
@@ -36,7 +33,16 @@ extension UINavigationController {
         var temp = viewControllers
         temp.removeAll([controller])
         setViewControllers(temp, animated: animated)
-     }
+    }
+    
+    /// 移除多个视图控制器
+    /// - Parameter controllers: 控制器集合
+    /// - Parameter animated: 是否动画
+    public func remove(_ controllers: [UIViewController], animated: Bool = false) {
+        var temp = viewControllers
+        temp.removeAll(controllers)
+        setViewControllers(temp, animated: animated)
+    }
 }
 
 fileprivate extension Array where Element: Equatable {
