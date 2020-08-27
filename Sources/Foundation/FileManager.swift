ff6c475f478a41b99b64f2685bc67449f81ac593
Lee <18611401994@163.com>
2020-08-27 14:35:15 +0800

    增加FileManager扩展 获取文件大小

---
+++ Sources/Foundation/FileManager.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension FileManager {
+    
+    /// 获取文件大小
+    /// - Parameter path: 路径
+    /// - Returns: Bytes
+    func fileSize(at path: String) -> Int64? {
+        var isDirectory: ObjCBool = false
+        guard fileExists(atPath: path, isDirectory: &isDirectory) else {
+            return nil
+        }
+        
+        if isDirectory.boolValue {
+            return subpaths(atPath: path)?.reduce(into: 0) {
+                $0 += (fileSize(at: path + "/" + $1) ?? 0)
+            }
+            
+        } else {
+            let attributes = try? attributesOfItem(atPath: path)
+            return attributes?[.size] as? Int64
+        }
+    }
+}
