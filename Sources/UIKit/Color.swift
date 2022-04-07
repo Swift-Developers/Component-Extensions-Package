463aa6ae92d5834ae51e0034b6111936564e9e3c
fun <lw_fun@163.com>
2022-04-07 12:14:33 +0800

    优化扩展

--- Sources/UIKit/Color.swift
+++ Sources/UIKit/Color.swift
@@ -57,21 +57,32 @@ extension UIColor {
 extension UIColor {
     
     public convenience init(hexString: String, alpha: CGFloat = 1.0) {
-        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let r, g, b, a: CGFloat
+        
         let scanner = Scanner(string: hexString)
-        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
-        var color: UInt64 = 0
-        scanner.scanHexInt64(&color)
-        let mask = 0x0000_00FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-        let red = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue = CGFloat(b) / 255.0
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        if #available(iOS 13.0, *) {
+            _ = scanner.scanCharacters(from: CharacterSet(charactersIn: "#"))
+        } else {
+            scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
+        }
+        var hexNumber: UInt64 = 0
+        scanner.scanHexInt64(&hexNumber)
+        if hexString.length > 7 {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+            
+            self.init(red: r, green: g, blue: b, alpha: a)
+        } else {
+            r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+            g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+            b = CGFloat(hexNumber & 0x0000ff) / 255
+            
+            self.init(red: r, green: g, blue: b, alpha: alpha)
+        }
     }
-
+    
     @available(iOS 13.0, *)
     public convenience init(dark: UIColor, light: UIColor, elevatedDark: UIColor? = nil, elevatedLight: UIColor? = nil) {
         self.init { trait in
@@ -90,26 +101,26 @@ extension UIColor {
             }
         }
     }
-
+    
     @available(iOS 13.0, *)
     public var lightMode: UIColor {
         resolvedColor(with: UITraitCollection(userInterfaceStyle: .light))
     }
-
+    
     @available(iOS 13.0, *)
     public var darkMode: UIColor {
         resolvedColor(with: UITraitCollection(userInterfaceStyle: .dark))
     }
-
+    
     public var isWhite: Bool {
         hexString == UIColor.white.hexString
     }
-
+    
     @available(iOS 13.0, *)
     public var inverted: UIColor {
         UIColor(dark: lightMode, light: darkMode)
     }
-
+    
     public var alpha: CGFloat {
         var r: CGFloat = 0
         var g: CGFloat = 0
@@ -118,28 +129,41 @@ extension UIColor {
         getRed(&r, green: &g, blue: &b, alpha: &a)
         return a
     }
-
+    
     public var hexString: String {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var a: CGFloat = 0
-
+        
         getRed(&r, green: &g, blue: &b, alpha: &a)
-
+        
         let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
-
+        
         return String(format: "#%06x", rgb)
     }
-
+    
+    public var hexStringWithAlpha: String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgba: Int = (Int)(r * 255) << 24 | (Int)(g * 255) << 16 | (Int)(b * 255) << 8 | (Int)(a * 255) << 0
+        
+        return String(format: "#%08x", rgba)
+    }
+    
     public func lighter(amount: CGFloat = 0.2) -> UIColor {
         return mixWithColor(UIColor.white, amount: amount)
     }
-
+    
     public func darker(amount: CGFloat = 0.2) -> UIColor {
         return mixWithColor(UIColor.black, amount: amount)
     }
-
+    
     public func mixWithColor(_ color: UIColor, amount: CGFloat = 0.25) -> UIColor {
         var r1: CGFloat = 0
         var g1: CGFloat = 0
@@ -149,7 +173,7 @@ extension UIColor {
         var g2: CGFloat = 0
         var b2: CGFloat = 0
         var alpha2: CGFloat = 0
-
+        
         getRed(&r1, green: &g1, blue: &b1, alpha: &alpha1)
         color.getRed(&r2, green: &g2, blue: &b2, alpha: &alpha2)
         return UIColor(
@@ -158,7 +182,7 @@ extension UIColor {
             blue: b1 * (1.0 - amount) + b2 * amount,
             alpha: alpha1)
     }
-
+    
     @available(iOS 13.0, *)
     public func dynamicMixWithColor(_ color: UIColor, amount: CGFloat = 0.25) -> UIColor {
         UIColor { trait in
@@ -169,10 +193,10 @@ extension UIColor {
             }
         }
     }
-
+    
     public var brightness: CGFloat {
         let originalCGColor = cgColor
-
+        
         // Now we need to convert it to the RGB colorspace. UIColor.white / UIColor.black are greyscale and not RGB.
         // If you don't do this then you will crash when accessing components index 2 below when evaluating greyscale colors.
         let RGBCGColor = originalCGColor.converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)
@@ -182,10 +206,10 @@ extension UIColor {
         guard components.count >= 3 else {
             return 0
         }
-
+        
         return ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
     }
-
+    
     public func isLight(threshold: CGFloat = 0.7) -> Bool {
         return brightness > threshold
     }
